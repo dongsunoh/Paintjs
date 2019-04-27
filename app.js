e49b439dab5b5ca9705a6e741d506f547aa8294b
Dongsun Oh <dongsun.oh@gmail.com>
2019-04-27 22:33:43 +0200

    added canvas drawing

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 // selected canvas selector
 const canvas = document.querySelector("#jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
+
+function startPainting(){
+    painting = true;
+}
 
 function stopPainting(){
     painting = false;
@@ -13,20 +24,25 @@ function onMouseMove(event) {
     // 캔버스 위치 값
     const x = event.offsetX;    
     const y = event.offsetY;
+
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+
 }
 
 function onMouseDown(event) {
     painting = true;
 }
 
-function onMouseUp(event) {
-    stopPainting();
-}
-
 // be of canvas selector value. 
 if( canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
